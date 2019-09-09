20dba7ae7286d7856a468f2994a5bc8fa1a0a43d
Marco Silva <marcoaurelio.fs@hotmail.com>
2019-09-09 19:21:29 -0300

    Correções no código para o desafio.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:marcador_truco/views/home_page.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Marcador de Truco',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: HomePage(),
+    ),
+  );
+}
